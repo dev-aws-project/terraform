0cd7f8a37e62684555a60827b953037493cf2b71
yury <yury.market.ma@gmail.com>
2023-06-04 18:17:13 +0200

    makes some changes in s3backend

--- live/dev/container-atlantis-server/config.tf
+++ live/dev/container-atlantis-server/config.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "s3" {
-    bucket         = "yury-aws-project-terraform-remote-state"
-    key            = "aws-project/terraform/live/dev/container-atlantis-server.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "yury-aws-project-terraform-remote-state"
+#     key            = "aws-project/terraform/live/dev/container-atlantis-server.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform-state-lock"
+#   }
+# }
 
 terraform {
   required_providers {
@@ -13,7 +13,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.59.0"
     }
-       
+
     github = {
       source = "integrations/github"
     }
